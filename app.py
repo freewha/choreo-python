2c2c911722624d5dc8a7c7c5534a11a7ea62adfb
freewha <freewha@kdwz.cloudns.ph>
2025-10-29 10:13:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,5 @@ def updateReservation(reservationId, request):
     return reservationId
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
